a1e0dcd4aa4536e8c877b0d548476b2c1568b8ba
Vytenis V Kazlas <vytenis.kazlas@gmail.com>
2017-09-06 21:30:58 +0300

    JS
    Adding albums JS JSON Button connection

--- script.js
+++ script.js
@@ -1,55 +1,133 @@
-// Kintamasis h1 title elemento keitimui
+// Suraskite H1 DOM elementą pagal klasę ir priskirkime jį kintamąjam
 let titleElement = document.getElementsByClassName("main-title")[0];
 
-// H1 pavadinimo keitimas
-titleElement.innerHTML = "Muzoniuks";
 
-//Pridedam papildoma klase
+// Pakeiskime H1 title pavadinima
+titleElement.innerHTML = "Muzikos albumai";
+
+
+// Pridėkime padildomą klasę H1
 titleElement.classList.add("header-title");
 
-//Pakeiskim stiliu - mygtuko spalva
+
+// Pakeiskime stiliųs
 titleElement.style.fontStyle = "italic";
 
-//Kai paspaudzia addAlbum
-// function addAlbum() {
-//     console.log ("pridedu Albuma");
-// };
 
-// let addAlbumButton = document.getElementsByClassName(".btn-save-album")[0];
+
+// Surandame formos mygtuką "Pridėti"
+// suras pirmą DOM elementą su klase .btn-save-album
 let addAlbumButton = document.querySelector(".btn-save-album");
 
+// metodas .querySelectorAll() suranda visus elementus su klase .btn-save-album
+// let addAlbumButton = document.querySelectorAll(".btn-save-album");
+
+// Kai paspaudžiamas mygtukas, vykdyk addAlbum funkciją
 addAlbumButton.addEventListener("click", addAlbum);
 
-// function addAlbum() {
-//     console.log ("pridedu Albuma");
-// };
 
-//album listas
-let albumList = [];
+// Čia saugosim visus albumus
+let albumsJSON = localStorage.getItem("albums");
+let albumList = JSON.parse(albumsJSON);
 
-//Formos elementai
+if (albumList === null) {
+    albumList = [];
+}
+
+
+// Formos elementai
 let artistElement = document.querySelector("#artist");
 let albumElement = document.querySelector("#album");
 let releaseDateElement = document.querySelector("#releaseDate");
 let imageElement = document.querySelector("#image");
 
+
 function addAlbum() {
-    //Nuskaitom reiksmes
+    // nuskaitome laukelių reikšmes
     let artist = artistElement.value;
     let album = albumElement.value;
     let date = releaseDateElement.value;
     let image = imageElement.files[0];
-    console.log("Atlikejas", artist, album, date, image);
-    
+
     if (!artist || !album || !date || !image) {
-        alert("Neivesti laukeliai");
+        console.log("Neįvesti laukeliai");
         return;
     }
 
-    albumList.push({
-        "Artist": artist,
-        "Album": album,
-        "Release_date": date,
-        "Cover": image.name
-    });
+    let record = {
+        "artist": artist,
+        "album": album,
+        "date": date,
+        "image": image.name
+    }
+
+    // Patikrinime ar albumas jau buvo įvestas
+    if (checkForDuplicates(record)){
+        alert("Toks albumas jau išsaugotas");
+        return;
+    }
+
+    // Saugome albumą
+    albumList.push(record);
+
+    // Issaugos duomenis i LocalStorage
+    let albumsJson = JSON.stringify(albumList);
+
+    localStorage.setItem("albums", albumsJson)
+    
+    renderAlbums();
+    console.log("albumsJSON:", albumsJSON);
+    // let parsedJson = JSON.parse(json);
+
+
+    console.log("Duomenys:", artist, album, date, image);    
 }
+
+function checkForDuplicates(record) {
+    let isDuplicate = false;
+    albumList.forEach(function(item){
+        if (item.artist === record.artist && item.album === record.album && item.date === record.date) {
+            isDuplicate = true;
+        }
+    })
+    return isDuplicate;
+};
+
+//Duomenu atvaizdavimas
+// let albumListElement = document.querySelector(".album-list");
+
+// let result = `
+//     <div class="album clearfix">
+//         <img src="img/R-365109-1390519203-2623.jpeg.jpg" alt="Puch zy bution">
+//         <h2>Chemical bros <span>Push the button</span></h2>
+//         <date>2004</date>
+//     </div>
+//     <hr>
+// `;
+
+function renderAlbums() {
+    
+    if(!albumList) return;
+
+    let result = "";
+    for (let album of albumList) {
+    result += `
+            <div class="album clearfix">
+                <img src="img/${album.image}" alt="${album.artist}">
+                <h2>${album.album}<span>${album.artist}</span></h2>
+                <date>${album.date}</date>
+            </div>
+            <hr>
+        `;
+    }
+
+   
+
+    let albumListElement = document.querySelector(".album-list");
+    albumListElement.innerHTML = result;
+
+}
+    
+
+
+
