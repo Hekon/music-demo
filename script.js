0632c5abe1d8cde07b6039e3197fee3edf0adbfa
Vytenis V Kazlas <vytenis.kazlas@gmail.com>
2017-09-13 21:41:22 +0300

    Sortas
    sortas js and stuff

--- script.js
+++ script.js
@@ -1,133 +1,287 @@
-// Suraskite H1 DOM elementą pagal klasę ir priskirkime jį kintamąjam
-let titleElement = document.getElementsByClassName("main-title")[0];
+var app = (function () {
 
+    // Surandame formos mygtuką "Pridėti"
+    // suras pirmą DOM elementą su klase .btn-save-album
+    let addAlbumButton = document.querySelector(".btn-save-album");
 
-// Pakeiskime H1 title pavadinima
-titleElement.innerHTML = "Muzikos albumai";
+    // Kai paspaudžiamas mygtukas, vykdyk addAlbum funkciją
+    addAlbumButton.addEventListener("click", addAlbum);
 
+    // Čia saugosim visus albumus
+    let allAlbums = [];
+    let httpRequest;
 
-// Pridėkime padildomą klasę H1
-titleElement.classList.add("header-title");
+    function init() {
+        // Gausime albumus iš json-server
+        httpRequest = new XMLHttpRequest();
 
-
-// Pakeiskime stiliųs
-titleElement.style.fontStyle = "italic";
-
-
-
-// Surandame formos mygtuką "Pridėti"
-// suras pirmą DOM elementą su klase .btn-save-album
-let addAlbumButton = document.querySelector(".btn-save-album");
-
-// metodas .querySelectorAll() suranda visus elementus su klase .btn-save-album
-// let addAlbumButton = document.querySelectorAll(".btn-save-album");
-
-// Kai paspaudžiamas mygtukas, vykdyk addAlbum funkciją
-addAlbumButton.addEventListener("click", addAlbum);
-
-
-// Čia saugosim visus albumus
-let albumsJSON = localStorage.getItem("albums");
-let albumList = JSON.parse(albumsJSON);
-
-if (albumList === null) {
-    albumList = [];
-}
-
-
-// Formos elementai
-let artistElement = document.querySelector("#artist");
-let albumElement = document.querySelector("#album");
-let releaseDateElement = document.querySelector("#releaseDate");
-let imageElement = document.querySelector("#image");
-
-
-function addAlbum() {
-    // nuskaitome laukelių reikšmes
-    let artist = artistElement.value;
-    let album = albumElement.value;
-    let date = releaseDateElement.value;
-    let image = imageElement.files[0];
-
-    if (!artist || !album || !date || !image) {
-        console.log("Neįvesti laukeliai");
-        return;
-    }
-
-    let record = {
-        "artist": artist,
-        "album": album,
-        "date": date,
-        "image": image.name
-    }
-
-    // Patikrinime ar albumas jau buvo įvestas
-    if (checkForDuplicates(record)){
-        alert("Toks albumas jau išsaugotas");
-        return;
-    }
-
-    // Saugome albumą
-    albumList.push(record);
-
-    // Issaugos duomenis i LocalStorage
-    let albumsJson = JSON.stringify(albumList);
-
-    localStorage.setItem("albums", albumsJson)
-    
-    renderAlbums();
-    console.log("albumsJSON:", albumsJSON);
-    // let parsedJson = JSON.parse(json);
-
-
-    console.log("Duomenys:", artist, album, date, image);    
-}
-
-function checkForDuplicates(record) {
-    let isDuplicate = false;
-    albumList.forEach(function(item){
-        if (item.artist === record.artist && item.album === record.album && item.date === record.date) {
-            isDuplicate = true;
+        if (!httpRequest) {
+            alert("Naršyklė nepalaiko AJAX");
+        } else {
+            httpRequest.onreadystatechange = processAlbumJson;
+            httpRequest.open('GET', 'http://localhost:3000/albums');
+            httpRequest.send();
         }
-    })
-    return isDuplicate;
-};
-
-//Duomenu atvaizdavimas
-// let albumListElement = document.querySelector(".album-list");
-
-// let result = `
-//     <div class="album clearfix">
-//         <img src="img/R-365109-1390519203-2623.jpeg.jpg" alt="Puch zy bution">
-//         <h2>Chemical bros <span>Push the button</span></h2>
-//         <date>2004</date>
-//     </div>
-//     <hr>
-// `;
-
-function renderAlbums() {
-    
-    if(!albumList) return;
-
-    let result = "";
-    for (let album of albumList) {
-    result += `
-            <div class="album clearfix">
-                <img src="img/${album.image}" alt="${album.artist}">
-                <h2>${album.album}<span>${album.artist}</span></h2>
-                <date>${album.date}</date>
-            </div>
-            <hr>
-        `;
     }
 
-   
+    // Kai gauname albumus iš json-server
+    function processAlbumJson() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                let albumsJSON = httpRequest.responseText;
+                let albumList = JSON.parse(albumsJSON);
 
-    let albumListElement = document.querySelector(".album-list");
-    albumListElement.innerHTML = result;
+                // Patikrinam ar sėkmingai pavyko sukurti naują objektą
+                if (albumList === null) {
+                    albumList = [];
+                }
 
-}
+                // Spausdinam albumus
+                allAlbums = albumList;
+                renderAlbums(albumList);
+
+            } else {
+                alert("Klaida. Negavau duomenų iš serverio");
+            }
+        }
+    }
+
+
+    // Formos elementai
+    let artistElement = document.querySelector("#artist");
+    let albumElement = document.querySelector("#album");
+    let releaseDateElement = document.querySelector("#releaseDate");
+    let imageElement = document.querySelector("#image");
+    let genreElement = document.querySelector("#genre");
+
+
+    function addAlbum() {
+        // nuskaitome laukelių reikšmes
+        let artist = artistElement.value;
+        let album = albumElement.value;
+        let date = releaseDateElement.value;
+        let image = imageElement.files[0];
+        let genre = genreElement.value;
+
+        if (!artist || !album || !date) {
+            alert("Neįvesti visi laukeliai");
+            return;
+        }
+
+        if (!isAlbumYear(date)) {
+            alert("Albumo metai įvesti neteisingai");
+            return;
+        };
+
+        let record = {
+            "artist": artist,
+            "album": album,
+            "date": date,
+            "image": image ? image.name || "" : "",
+            "genre": genre
+        }
+
+        // Patikrinime ar albumas jau buvo įvestas
+        if (checkForDuplicates(record)) {
+            alert("Toks albumas jau išsaugotas");
+            return;
+        }
+
+
+        // Siunčiame albumo duomenis į serverį
+        let httpRequest = new XMLHttpRequest();
+
+        if (!httpRequest) {
+            alert("Naršyklė nepalaiko AJAX");
+        } else {
+            httpRequest.onreadystatechange = saveAlbumToServer;
+            httpRequest.open('POST', 'http://localhost:3000/albums');
+            httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            httpRequest.send("artist=" + record.artist + "&album=" + record.album + "&date=" + record.date + "&image=" + record.image + "&genre=" + record.genre);
+        }
+
+        function saveAlbumToServer() {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 201) {
+                    console.log("Išsaugojau sėkmingai");
+
+                    // Spausdinam albumus
+                    allAlbums.push(record);
+                    renderAlbums(allAlbums);
+                } else {
+                    alert("Negaliu išsaugoti serverio");
+                }
+            }
+        }
+
+        // Išvalykim formą
+        clearForm();
+    }
+
+    function checkForDuplicates(record) {
+        let isDuplicate = false;
+        allAlbums.forEach(function (item) {
+            if (item.artist === record.artist && item.album === record.album && item.date === record.date) {
+                isDuplicate = true;
+            }
+        })
+        return isDuplicate;
+    };
+
+    function isAlbumYear(year) {
+        let date = new Date();
+        let currentYear = date.getFullYear();
+
+        if (isFinite(year) && year <= currentYear && year > 1900) {
+            return true
+        } else {
+            return false;
+        }
+    }
+
+    let formElement = document.querySelector(".album-form");
+    let clearButton = document.querySelector(".btn-clear-form");
+
+    clearButton.addEventListener("click", clearForm);
+
+    function clearForm() {
+        formElement.reset();
+    }
+
+    function renderAlbums(albumList) {
+        // Patikrinti ar yra išsaugotų albumų
+        // Jei nėra - nutraukti funkcijos vykdymą
+        if (!albumList) return;
+
+        // Jei yra, kuriame ciklą ir į rezultatą susidedam visų albumų HTML
+        let albumHtml = "";
+        for (let album of albumList) {
+
+            let genreHtml = "";
+
+            if (album.genre) {
+                let genreList = (album.genre).split(",");
+
+                for (genre of genreList) {
+                    genreHtml += `<span class="badge badge-pill badge-info">${genre.trim()}</span>\n`;
+                }
+            }
+
+            albumHtml += `
+            <div class="album clearfix">
+                <img src="img/${album.image}" alt="${album.artist} - ${album.album}">
+                <h2>${album.album} - ${album.artist} <span class="text-secondary">(${album.date})</span></h2>
+                ${genreHtml}
+                <div class="text-right mt-5">
+                    <button type="button" class="btn btn-light btn-delete-album" data-id="${album.id}">Ištrinti</button>
+                </div>
+                
+            <hr>
+            </div>
+        `;
+        }
+
+        // Spausdinam rezultatą į ".album-list"
+        let albumListElement = document.querySelector(".album-list");
+        albumListElement.innerHTML = albumHtml;
+
+        // Surandam visus mygtukus
+        let deleteButtons = document.querySelectorAll(".btn-delete-album");
+
+        // Registruojam mygtukams event'us
+        for (btn of deleteButtons) {
+            btn.addEventListener("click", deleteAlbum);
+        }
+    }
+
+    function deleteAlbum() {
+        let doDelete = confirm("Ar tikrai norite istrinti?");
+        if (doDelete) {
+            let id = this.getAttribute("data-id");
+            // console.log("trinsim albuma cia", this);
+
+            this.parentElement.parentElement.remove();
+            // $(this).parents(".album").remove();
+
+            deleteAlbumFromDb(id);
+        }
+    }
+
+    function deleteAlbumFromDb(id) {
+        let httpRequest = new XMLHttpRequest();
+
+        if (!httpRequest) {
+            alert("Naršyklė nepalaiko AJAX");
+        } else {
+            httpRequest.onreadystatechange = deleteAlbumCallback;
+            httpRequest.open('DELETE', 'http://localhost:3000/albums/' + id);
+            httpRequest.send();
+        }
+
+    }
     
+    function deleteAlbumCallback() {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                console.log("Albumas sekmingai istrintas");
+            } else {
+                console.log("Klaida");
+            }
+        }
+    }
+
+    let sortByElement = document.querySelector(".sort-by");
+    let sortBtnElement = document.querySelector(".btn-sort");
+    let sortBy = sortByElement.value;
+
+    sortBtnElement.addEventListener("click", sortAlbumsClick);
+    function sortAlbumsClick() {
+        let albumsSorted;
+
+        if (sortBy === "album") {
+            albumsSorted = allAlbums.sort(sortByAlbum);
+        } else if (sortBy === "artist") {
+            albumsSorted = allAlbums.sort(sortByArtist);
+        } else {
+            albumsSorted = allAlbums.sort(sortByYear);
+        }
+
+        
+
+        renderAlbums(albumsSorted);
+
+        console.log("Rikiuok pagal: ", sortBy);
+    }
+
+    function sortByAlbum(a, b) {
+        if (a.album > b.album){
+            return 1;
+        }
+
+        return -1;
+    }
+
+    function sortByYear(a, b) {
+        if (a.date > b.date){
+            return 1;
+        }
+
+        return -1;
+    }
+
+    function sortByArtist(a, b) {
+        if (a.artist > b.artist){
+            return 1;
+        }
+
+        return -1;
+    }
 
 
+    return {
+        "init": init
+    }
 
+})();
+
+app.init();
